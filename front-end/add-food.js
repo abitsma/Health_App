2eddc5bbf722f922d7b11df269d0e14d95ad962e
benwayland-cell <ben.wayland2007@gmail.com>
2026-06-14 16:34:02 -0600

    Made js for add-food that could interface with back end

--- front-end/add-food.js
+++ front-end/add-food.js
@@ -1,1 +1,31 @@
 
+
+const manualEntryFormElement = document.getElementById("manualEntryForm");
+
+/*
+    Saves the food to the back end
+*/
+function submitFood(event) {
+    event.preventDefault();
+
+    const newFoodObject = {
+        name: manualEntryFormElement.name.value,
+        calories: manualEntryFormElement.calories.value,
+        protein: manualEntryFormElement.protein.value,
+        carbs: manualEntryFormElement.carbs.value,
+        fat: manualEntryFormElement.fat.value,
+    }
+    
+    const mealType = manualEntryFormElement.mealType.value;
+
+    console.log("Food Object: ", newFoodObject);
+    console.log("Meal Type: ", mealType);
+
+    /*
+                                                    TO PEOPLE TRYING TO INTERFACE WITH FRONT END
+        
+        This is where you can have the javascript give the server a new meal
+    */
+}
+
+manualEntryFormElement.addEventListener("submit", submitFood);
