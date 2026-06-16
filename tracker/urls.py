fcda4557f258ab5de779d2400ebc3a5e1e47ccff
Troy Justesen <tjustesen08@gmail.com>
2026-06-16 13:07:26 -0600

    making changes to log page

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-#Connecting pages to tracker requests
+# Connecting pages to tracker requests
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("log/", views.log, name="log"),
     path("add-food/", views.add_food, name="add_food"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -37,13 +37,40 @@ def dashboard(request):
 
     return render(request, "tracker/index.html", context)
 
+
+# Log Page View
+def log(request):
+    today = timezone.now().date()
+
+    entries = FoodEntry.objects.filter(
+        date_added=today
+    )
+
+    meals = {
+        "breakfast": entries.filter(meal_type="breakfast"),
+        "lunch": entries.filter(meal_type="lunch"),
+        "dinner": entries.filter(meal_type="dinner"),
+        "snack": entries.filter(meal_type="snack"),
+    }
+
+    total_calories = sum(entry.calories for entry in entries)
+
+    context = {
+        "meals": meals,
+        "total_calories": total_calories,
+        "calorie_goal": 2000,
+        "remaining": 2000 - total_calories,
+    }
+
+    return render(request, "tracker/log.html", context)
+
+
 def add_food(request):
     food_name = request.POST.get("food_name")
     calories = request.POST.get("calories")
     meal_type = request.POST.get("meal_type")
 
     FoodEntry.objects.create(
-        user=request.user,
         food_name=food_name,
         calories=calories,
         meal_type=meal_type
